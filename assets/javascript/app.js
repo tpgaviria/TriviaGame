22d5542fd964202ea98644b85f9a6a36f81e853c
TGAV <46203536+tpgaviria@users.noreply.github.com>
2019-03-25 12:26:45 -0400

    initial files and directories

---
+++ assets/javascript/app.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+    // global variables
+    var questionCounter = 0;
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+    var time = 15;
+
+    var questionArray = [
+        {
+            question: "What is Earth's largest continent?"
+            choices: [""]
+
+        },
+        {
+            question: "What river runs through Baghdad?"
+            choice: []
+        },
+        {
+            question: "What country has the most natural lakes?"
+            choice: []
+        },
+        {
+            question: "What is the driest place on Earth?"
+            choice: []
+        },
+        {
+            question: "What is the oldest city in the world?"
+            choice: []
+        },
+        {
+            question: "What country has the most coastline?"
+            choice: []
+        },
+        {
+            question: "What is the largest country in South America?"
+            choice: []
+        },
+        {
+            question: "What is the tallest mountain in the world?"
+            choice: []
+        },
+        {
+            question: "What is the capital of Australia?"
+            choice: []
+        },
+        {
+            question: "What is the southernmost country on the Arabian Peninsula?"
+            choice: []
+        },
+        {
+            question: "Montevideo is the capital of what South American country?"
+            choice: []
+        },
+        {
+            question: "What is the only continent with land in all four hemispheres?"
+            choice: []
+        },
+        {
+            question: "What is the only major city located on two continents?"
+            choice: []
+        },
+        {
+            question: "What river runs through Paris?"
+            choice: []
+        },
+        {
+            question: ""
+            choice: []
+        },
+        {
+            question: ""
+            choice: []
+        },
+        {
+            question: ""
+            choice: []
+        },
+        {
+            question: ""
+            choice: []
+        },
+        {
+            question: ""
+            choice: []
+        },
+        {
+            question: ""
+            choice: []
+        },
+    
+    
+    ]
+
+
+
+}
